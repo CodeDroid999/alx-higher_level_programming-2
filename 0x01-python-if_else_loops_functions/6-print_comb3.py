07ff06a3469f5fe8f8830ff9cbe418c48329800d
rotich98 <emmanuelkipkemei13@gmail.com>
2022-09-20 12:07:51 -0700

    Bug fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{:02d}".format(i), end=', ')
-print("{:02d}".format(i + 1))
+for i in range(0, 9):
+    for j in range(i + 1, 10):
+        if i is not 8 or j is not 9:
+            print("{:d}{:d}".format(i, j), end=", ")
+        else:
+            print("{:d}{:d}".format(i, j))
