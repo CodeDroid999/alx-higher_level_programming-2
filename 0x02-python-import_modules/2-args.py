086659cf82876aea75aa4545639afe1e572927dc
rotich98 <emmanuelkipkemei13@gmail.com>
2022-09-22 08:52:51 -0700

    Bug fixed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print the number of and list of arhuments."""
+    """Print the number of and list of arguments."""
     import sys
 
-    count = len(sys.argv) -1
+    count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
     elif count == 1:
