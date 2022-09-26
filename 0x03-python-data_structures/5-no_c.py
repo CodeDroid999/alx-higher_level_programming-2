4cde6599dfff2d0ee03174f9ba4af82fc7387515
rotich98 <emmanuelkipkemei13@gmail.com>
2022-09-26 03:41:18 -0700

    Bug fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 
 def no_c(my_string):
     new_st = my_string.translate({ord('c'): None})
-    new_st = new_s.translate({ord('C'): None})
+    new_st = new_st.translate({ord('C'): None})
     return new_st
