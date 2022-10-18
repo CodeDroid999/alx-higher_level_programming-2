e19ddbf3d7e9ef1c63ed17c52c0e98f2ca2da5a4
rotich98 <emmanuelkipkemei13@gmail.com>
2022-10-17 20:54:54 -0700

    First commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+Defining an empty class Rectangle
+"""
+
+
+class Rectangle:
+    """An empty representation of a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Advanced classes and objects in python(0x08. Python - More Classes and Objects)
