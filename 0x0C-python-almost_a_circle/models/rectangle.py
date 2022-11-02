d0b699c0928725f3e4ef02f268e3af0a6133b7b0
rotich98 <emmanuelkipkemei13@gmail.com>
2022-11-02 12:26:33 -0700

    Bug fixed

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -154,4 +154,3 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
-
